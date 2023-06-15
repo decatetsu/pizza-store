b2ad9ee2626a576884a94588824bd33251f9c7db
Andrii Yefremov <56955307+ka1tzyu@users.noreply.github.com>
2023-06-15 11:41:44 +0300

    fix: remove comments and svg

--- src/js/index.js
+++ src/js/index.js
@@ -1,30 +1,15 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter, createBrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import store from '../redux/store';
 
 import '../scss/app.scss';
-import {Cart, Home} from './pages';
 import App from './App';
-
-const router = createBrowserRouter([
-    {
-        path: '/',
-        element: Home,
-    },
-    {
-        path: '/cart',
-        element: Cart,
-    }
-]);
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-
-// <Route exact path="/" element={Home} />
-// <Route path="/cart" element={Cart} />
 
 root.render(
     <Provider store={store}>
