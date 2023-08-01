303214c13e55b73c456c7e4ae82ce8441c1d1d5c
Andrii Yefremov <56955307+ka1tzyu@users.noreply.github.com>
2023-08-01 08:11:14 +0300

    fix: update router

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 
 import { Home, Cart } from './pages';
 import { Header } from './components';
 
 function App() {
-    return (
-        <div className="wrapper">
-            <Header />
-            <div className="content">
-                <Routes>
-                    <Route index element={<Home/>} />
-                    <Route path="/cart" element={<Cart/>} />
-                </Routes>
-            </div>
-        </div>
-    );
+  return (
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <HashRouter>
+          <Routes>
+            <Route index element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
+        </HashRouter>
+      </div>
+    </div>
+  );
 }
 
 export default App;
